437af8b62483fb6274e7150b5c34ff513e64af4b
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2022-02-07 07:44:14 +0300

    🎨 new design

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const html = document.querySelector('html'), sections = document.getElementsByTagName('section'), onScroll = () => {
-    for (const section of sections)
-        section.classList.remove('focus');
-    sections[Math.round((html.scrollTop / html.scrollHeight) * 4)].classList.add('focus');
-};
-window.addEventListener('scroll', onScroll);
-onScroll();
-//# sourceMappingURL=main.js.map
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-const html = document.querySelector('html') as HTMLElement,
-  sections = document.getElementsByTagName('section'),
-  onScroll = () => {
-    for (const section of sections) section.classList.remove('focus');
-    sections[Math.round((html.scrollTop / html.scrollHeight) * 4)].classList.add('focus');
-  };
-
-window.addEventListener('scroll', onScroll);
-onScroll();
